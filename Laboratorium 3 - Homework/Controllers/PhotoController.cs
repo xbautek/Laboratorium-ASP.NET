454e4430ec34682f0fc8b3f49557b57162c34e08
xbautek <bartosz.ksiazek@microsoft.wsei.edu.pl>
2023-10-24 19:04:20 +0200

    Homework done

---
+++ Laboratorium 3 - Homework/Controllers/PhotoController.cs
@@ -0,0 +1,78 @@
+﻿using Laboratorium_3___Homework.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium_3___Homework.Controllers
+{
+    public class PhotoController : Controller
+    {
+        static Dictionary<int, Photo> _photos = new Dictionary<int, Photo>();
+        static int id = 1;
+        public IActionResult Index()
+        {
+            return View(_photos);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Photo model)
+        {
+            if (ModelState.IsValid)
+            {
+                //dodaj model do bazy lub kolekcji
+                model.Id = id++;
+                _photos.Add(model.Id, model);
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return View(_photos[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Photo model)
+        {
+            _photos.Remove(model.Id);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(_photos[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Photo model)
+        {
+            if (ModelState.IsValid)
+            {
+                _photos[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            return View(_photos[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Details()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Laboratorium 3 - Homework/Models/Photo.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorium_3___Homework.Models
+{
+    public class Photo
+    {
+
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Data musi zostać wybrana!")]
+        [DataType(DataType.Date)]
+        public DateTime DateAndTime { get; set; }
+
+        [StringLength(70, ErrorMessage = "Opis jest za długi")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Musi być podana kamera!")]
+        public string Camera { get; set; }
+
+        [Required(ErrorMessage = "Musi być podany autor!")]
+        [StringLength(80, ErrorMessage = "Nazwa autora jest za długa")]
+        public string Author { get; set; }
+
+        [StringLength(25)]
+        public string? Resolution { get; set; }
+
+        [Required(ErrorMessage = "Musi być podany format!")]
+        [RegularExpression(@"^\d{1,3}x\d{1,3}$", ErrorMessage = "Taki format nie istnieje.")]
+        public string Format { get; set; }
+    }
+}
